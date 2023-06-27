f5196ffc75497864960a88a01f58bdd8078007e3
keerthi gondi <keerthisudha.gondi@gmail.com>
2023-06-27 08:17:12 -0500

    Added NodeJS Assignment1

---
+++ NodeJSassignment1/src/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs/promises')
+
+const myFileWriter = async (fileName, fileContent) => {
+	const data =await fs.writeFile(fileName,fileContent);
+}
+
+const myFileReader = async (fileName) => {
+	const data =await fs.readFile(fileName,"utf-8");
+	console.log(data);
+}
+
+const myFileUpdater = async (fileName, fileContent) => {
+	const data =await fs.appendFile(fileName,fileContent);
+	console.log(data);
+}
+
+const myFileDeleter = async (fileName) => {
+	const data =await fs.unlink(fileName);
+}
+
+myFileWriter("file1.txt","Hello");
+myFileReader("file1.txt");
+myFileUpdater("file1.txt","World");
+myFileDeleter("file1.txt")
+
+module.exports = { myFileUpdater, myFileReader, myFileDeleter }
